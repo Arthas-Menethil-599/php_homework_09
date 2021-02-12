1d6de990f225c7130ba5e61b609c446551f247c9
David <sir.baimyrza@gmail.com>
2021-02-12 20:51:50 +0600

    use class Matrix

---
+++ php_homeworks/php_homework_09/01.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . "/Matrix.php";
+
+
+$matrix1 = new Matrix([[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]);
+
+$matrix2 = new Matrix([[5,8,1],
+    [6,7,3],
+    [4,5,9]]);
+
+//Тесты
+//$result = $matrix2->add_or_diff($matrix1,'+');
+//$result = $matrix2->add_or_diff($matrix1,'-');
+//$result = $matrix1->mult($matrix2);
+
+//var_dump($result->get());
